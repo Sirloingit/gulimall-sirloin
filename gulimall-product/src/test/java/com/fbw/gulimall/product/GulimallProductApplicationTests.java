1af61255640ac9fb386551849caeb9bc9ebb6ab0
Sirloingit <55542459+Sirloingit@users.noreply.github.com>
2021-03-16 16:28:20 +0800

    Update GulimallProductApplicationTests.java

--- gulimall-product/src/test/java/com/fbw/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/fbw/gulimall/product/GulimallProductApplicationTests.java
@@ -26,8 +26,8 @@ public class GulimallProductApplicationTests {
 ////        // Endpoint以杭州为例，其它Region请按实际情况填写。
 ////        String endpoint = "oss-cn-shanghai.aliyuncs.com";
 ////        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-////        String accessKeyId = "LTAI4G5aaAyWWi9LrZ6jrR1M";
-////        String accessKeySecret = "tJTmEIhhCZKF1jVPSH5a0EIbISEOlk";
+////        String accessKeyId = "";
+////        String accessKeySecret = "";
 ////
 ////        // 创建OSSClient实例。
 ////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
